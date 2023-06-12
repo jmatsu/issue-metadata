c2fa3d7d171b3a3f3b61dd2af1897ce4de4b946a
Jumpei Matsuda <jmatsu.drm@gmail.com>
2023-06-12 13:19:18 +0900

    fix: allow embedding multiple matadata

--- __tests__/conventions.test.ts
+++ __tests__/conventions.test.ts
@@ -1,5 +1,24 @@
-import {anchor, extractValues, buildCommentBody} from '../src/conventions'
+import {anchor, extractValues, createMetadata, getMetadata, buildCommentBody, devider} from '../src/conventions'
 import {expect, test} from '@jest/globals'
+
+test('createMetadata embed a json object into a comment', async () => {
+  const comment = createMetadata({"bar": "bar_value"})
+
+  expect(comment).toEqual(`<!--
+{
+  "bar": "bar_value"
+}
+-->`)
+})
+
+test('getMetadata get a json object from the content', async () => {
+  const body = `<!--
+{ "bar": "bar_value" }
+-->`
+  const values = getMetadata(body)
+
+  expect(values['bar']).toEqual('bar_value')
+})
 
 test('buildCommentBody builds a body that contains anchor', async () => {
   const body = buildCommentBody('bar', 'bar_value', {})
@@ -14,22 +33,23 @@ test('extractValues can extract values from the built body', async () => {
   expect(values['bar']).toEqual('bar_value')
 })
 
-test('extractValues extracts values from the content', async () => {
-  const body = `brabrabra
-
-  ${anchor}
-  <!--
-  { "bar": "bar_value" }
-  -->`
-  const values = extractValues(body)
-
-  expect(values['bar']).toEqual('bar_value')
-})
-
 test('extractValues can extract all values', async () => {
   const body = buildCommentBody('bar', 'bar_value', {'foo': 'foo_value'})
   const values = extractValues(body)
 
   expect(values['bar']).toEqual('bar_value')
   expect(values['foo']).toEqual('foo_value')
+})
+
+test('extractValues can ignore metadats above of values', async () => {
+  const values = extractValues(`brabrabrba
+
+${anchor}
+${devider}
+<!-- { "metadata": "this must be ignored" } -->
+${devider}
+<!-- { "bar": "bar_value"} -->
+`)
+
+  expect(values['bar']).toEqual('bar_value')
 })
